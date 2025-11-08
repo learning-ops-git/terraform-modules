003ed041f1066682b034035725141ab21ba343f6
learning-ops-git <higuitadaniel94@gmail.com>
2025-11-07 19:10:54 -0500

    updated

--- module/s3/s3.tf
+++ module/s3/s3.tf
@@ -6,7 +6,19 @@ resource "aws_s3_bucket" "main" {
   tags = local.resource_tags
 }
 
+resource "aws_lambda_permission" "allow_s3_invocation" {
+  provider = aws.main
+  count    = (length(var.s3_notifications.lambda) > 0 ? length(var.s3_notifications.lambda) : 0)
+
+  statement_id  = "AllowS3Invocation-${count.index}"
+  action        = "lambda:InvokeFunction"
+  function_name = var.s3_notifications.lambda[count.index].function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.main.arn
+}
+
 resource "aws_s3_bucket_notification" "notificacion_s3" {
+  depends_on = [aws_lambda_permission.allow_s3_invocation]
   provider = aws.main
   bucket   = aws_s3_bucket.main.id
   count    = (length(var.s3_notifications.lambda) > 0 ? 1 : 0)
